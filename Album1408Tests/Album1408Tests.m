da1b5d6c46c90722d4405ef842bc5447ee368161
zhang_ying <zhang_ying@zhang-ying-no-MacBook-Pro.local>
2014-08-25 16:11:50 +0800

    Initial Commit

---
+++ Album1408Tests/Album1408Tests.m
@@ -0,0 +1,34 @@
+//
+//  Album1408Tests.m
+//  Album1408Tests
+//
+//  Created by zhang_ying on 14-8-25.
+//  Copyright (c) 2014年 zhang_ying. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Album1408Tests : XCTestCase
+
+@end
+
+@implementation Album1408Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
